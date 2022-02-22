67518a52e5ab8a4e15d9b81b20f1984866745141
Sanat Gawade <sanat.leo9@gmail.com>
2022-02-22 22:28:21 +0530

    added counter to this component

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementHandler = () => {
+    setCount(count + 1);
+  };
+
+  const decrementHandler = () => {
+    setCount(count - 1);
+  };
   return (
     <Fragment>
       <h1>Welcome home</h1>
+      <button onClick={incrementHandler}>+</button>
+      <h3>{count}</h3>
+      <button onClick={decrementHandler}>-</button>
     </Fragment>
   );
 };
